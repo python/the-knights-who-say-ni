f5be52e0b1a744308dcc3f408ba5ec6df81783bb
Brett Cannon <brett@python.org>
2016-02-05 15:05:29 -0800

    Don't cover default values from abc

--- ni/abc.py
+++ ni/abc.py
@@ -19,6 +19,7 @@ class ServerHost(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def port(self) -> int:
         """Specify the port to bind the listening socket to."""
+        raise NotImplementedError
 
 
 class ContribHost(metaclass=abc.ABCMeta):
@@ -28,26 +29,25 @@ class ContribHost(metaclass=abc.ABCMeta):
     @property
     @abc.abstractstaticmethod
     def route() -> t.Tuple[str, str]:
-        return '*', '/'
+        return '*', '/'  # pragma: no cover
 
     @abc.abstractclassmethod
     async def process(cls, request: t.Any) -> t.Optional['ContribHost']:
         """Process a request, returning None if there's nothing to do."""
-        return None
 
     @abc.abstractstaticmethod
     def nothing_to_do() -> web.StreamResponse:
         """Respond to the contribution, signaling there's nothing to do."""
-        return web.Response(status=501)
+        return web.Response(status=501)    # pragma: no cover
 
     @abc.abstractmethod
     async def usernames(self) -> t.Iterable[str]:
         """Return an iterable with all of the contributors' usernames."""
-        return []
+        return []    # pragma: no cover
 
     @abc.abstractmethod
     async def update(self, status: Status) -> web.StreamResponse:
-        return web.Response(status=501)
+        return web.Response(status=501)    # pragma: no cover
 
 
 class CLAHost(metaclass=abc.ABCMeta):
@@ -57,4 +57,4 @@ class CLAHost(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     async def check(self, usernames: t.Iterable[str]) -> Status:
         """Check if all of the specified usernames have signed the CLA."""
-        return Status.MISSING_USERNAME
+        return Status.MISSING_USERNAME    # pragma: no cover
