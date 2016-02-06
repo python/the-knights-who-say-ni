8a50f2d5a56371bf6db19ed4d31cfccf10111ce3
Brett Cannon <brettcannon@users.noreply.github.com>
2016-02-06 10:55:04 -0800

    Go back to accepting a web.Request for ContribHost.process()

--- ni/abc.py
+++ ni/abc.py
@@ -32,7 +32,7 @@ class ContribHost(metaclass=abc.ABCMeta):
         return '*', '/'  # pragma: no cover
 
     @abc.abstractclassmethod
-    async def process(cls, request: t.Any) -> t.Union['ContribHost', web.StreamResponse]:
+    async def process(cls, request: web.Request) -> t.Union['ContribHost', web.StreamResponse]:
         """Process a request, returning an instance of this class.
 
         If there is nothing to do (which includes errors), then an instance of
