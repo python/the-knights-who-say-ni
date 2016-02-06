688719abb1f44ff166ea7cae52942a011b69f2ab
Brett Cannon <brett@python.org>
2016-02-05 16:08:12 -0800

    Properly manage event loop lifetimes in tests

--- ni/test/test_github.py
+++ ni/test/test_github.py
@@ -16,14 +16,16 @@ class GitHubTests(unittest.TestCase):
         # GitHub can ping a webhook to verify things are set up.
         # https://developer.github.com/webhooks/#ping-event
         request = {'zen': 'something pithy'}
-        loop = asyncio.get_event_loop()
+        loop = asyncio.new_event_loop()
+        self.addCleanup(loop.close)
         result = loop.run_until_complete(github.Host.process(request))
         self.assertIsNone(result)
 
     def test_process_skipping(self):
         # Only create a ContibHost object if the PR is opened, unlabeled, or
         # synchronized.
-        loop = asyncio.get_event_loop()
+        loop = asyncio.new_event_loop()
+        self.addCleanup(loop.close)
         for event in github.PullRequestEvent:
             if event in self.acceptable:
                 continue
