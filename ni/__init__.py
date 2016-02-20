3566348975302cc2a30e7223f9b72ed6090556d1
Brett Cannon <brett@python.org>
2016-02-20 11:59:12 -0800

    Explain better why the abstraction/decoupling is so stringent

--- ni/__init__.py
+++ ni/__init__.py
@@ -1,4 +1,11 @@
-# Abstract out the host implementations to prevent tight coupling.
+"""Abstract out the host implementations to prevent tight coupling.
+
+This makes it easier to swap out parts of the cade without affecting other
+sections, making it easy to change hosts when needs change. As a side-effect,
+this also makes forks with different backend needs easier to manage as they
+will only need to change this file and add their own implementations to their
+fork.
+"""
 from .bpo import Host as CLAHost
 from .github import Host as ContribHost
 from .heroku import Host as ServerHost
