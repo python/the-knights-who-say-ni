5b9c7723e00ec1bb15e6137351aba4f0100d6595
Brett Cannon <brettcannon@users.noreply.github.com>
2017-02-13 15:19:50 -0800

    Use -m with mypy

--- mypy.sh
+++ mypy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mypy --python-version 3.6 --fast-parser --strict-optional --ignore-missing-imports --warn-unused-ignores --warn-redundant-casts --disallow-untyped-defs --disallow-untyped-calls ni/*.py
+python3 -m mypy --python-version 3.6 --fast-parser --strict-optional --ignore-missing-imports --warn-unused-ignores --warn-redundant-casts --disallow-untyped-defs --disallow-untyped-calls ni/*.py
