f67eedfa0a4ba62242f7020eab44d11830248fae
Brett Cannon <brettcannon@users.noreply.github.com>
2018-04-13 17:58:24 -0700

    Don't hard-code the Python version for mypy (#143)

--- mypy.sh
+++ mypy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python3 -m mypy --python-version 3.6 --strict-optional --ignore-missing-imports --warn-unused-ignores --warn-redundant-casts --disallow-untyped-defs --disallow-untyped-calls ni/*.py
+python3 -m mypy --strict-optional --ignore-missing-imports --warn-unused-ignores --warn-redundant-casts --disallow-untyped-defs --disallow-untyped-calls ni/*.py
