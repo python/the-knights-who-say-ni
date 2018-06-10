be76a78fea5b57042d509645e0e3fd96a041e9d5
Andrés Delfino <adelfino@gmail.com>
2018-06-10 13:52:18 -0300

    Fix a typo. Also, don't repeat "look" (#147)

--- ni/github.py
+++ ni/github.py
@@ -38,7 +38,7 @@ When your account is ready, please add a comment in this pull request
 and a Python core developer will remove the `CLA not signed` label
 to make the bot check again.
 
-Thanks again to your contribution and we look forward to looking at it!
+Thanks again for your contribution, we look forward to reviewing it!
 """
 
 NO_CLA_BODY = """Unfortunately our records indicate you have not signed the CLA. \
